2af1344217053ff48d7598b150a9e0f8e214649a
Shen Chen <shenchen@ubuntu.(none)>
2013-07-30 10:57:58 +0800

    add

--- checkin/handler.py
+++ checkin/handler.py
@@ -3,6 +3,7 @@
 import json
 import config
 import time
+import urllib
 import tornado
 import checkin.network
 
@@ -14,17 +15,18 @@ _KEY = "FEED"
 class FeedHandler(tornado.web.RequestHandler):
     def get(self):
         data = []
-        for item in  _client.zrange(_KEY, 0, 10, withscores = True):
+        for item in  _client.zrevrange(_KEY, 0, 10, withscores = True):
             j_obj = json.loads(item[0])
-            data.append((j_obj['data'], int(item[1]), float(j_obj['lo']), float(j_obj['la'])))
+            data.append({'data':j_obj['data'], 'created':int(item[1]), 'lat':float(j_obj['lat']), 'lng':float(j_obj['lng']), 'name':j_obj['name']})
         self.set_header('Content-Type',"application/json" )
         self.write(json.dumps(data))
 
     def post(self):
-        data = self.get_argument('data', 'No data received')
-        lo   = self.get_argument('lo', -1)
-        la   = self.get_argument('la', -1)
-        _client.zadd(_KEY, json.dumps({'lo':lo, 'la':la, 'data':data}), int(time.time()))
+        data = self.get_argument('data', '')
+        name = self.get_argument('name', '')
+        lng   = self.get_argument('lng', -1)
+        lat   = self.get_argument('lat', -1)
+        _client.zadd(_KEY, json.dumps({'lng':lng, 'lat':lat, 'data':data, 'name':name}), int(time.time()))
         self.write("{}")
 
 class NearbyHandler(tornado.web.RequestHandler):
@@ -35,10 +37,10 @@ class NearbyHandler(tornado.web.RequestHandler):
         offset  = int(self.get_argument('offset', 0))
         limit  = int(self.get_argument('limit', 12))
         data = []
-        url = config.NEARBY_API+ "?types=food&rankby=distance&language=zh-CN&sensor=true&location=%s,%s&key=%s"%(lat,lng,config.API_KEY)
+        url = config.NEARBY_API+ "?types=(accounting|airport|amusement_park|aquarium|art_gallery|atm|bakery|bank|bar|beauty_salon|bicycle_store|book_store|bowling_alley|bus_station|cafe|campground|car_dealer|car_rental|car_repair|car_wash|casino|cemetery|church|city_hall|clothing_store|convenience_store|courthouse|dentist|department_store|doctor|electrician|electronics_store|embassy|establishment|finance|fire_station|florist|food|funeral_home|furniture_store|gas_station|general_contractor|grocery_or_supermarket|gym|hair_care|hardware_store|health|hindu_temple|home_goods_store|hospital|insurance_agency|jewelry_store|laundry|lawyer|library|liquor_store|local_government_office|locksmith|lodging|meal_delivery|meal_takeaway|mosque|movie_rental|movie_theater|moving_company|museum|night_club|painter|park|parking|pet_store|pharmacy|physiotherapist|place_of_worship|plumber|police|post_office|real_estate_agency|restaurant|roofing_contractor|rv_park|school|shoe_store|shopping_mall|spa|stadium|storage|store|subway_station|synagogue|taxi_stand|train_station|travel_agency|university|veterinary_care|zoo)&rankby=distance&language=zh-CN&sensor=true&location=%s,%s&key=%s"%(lat,lng,config.API_KEY)
         if query is not None:
+            query = urllib.quote(query.encode('utf8'))
             url = url + "&name=%s"%(query)
-        print url
         result = checkin.network.https_get(url) 
         j_obj = json.loads(result) 
         for idx, item in enumerate(j_obj.get("results", [])):
